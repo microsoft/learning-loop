032b39bca80589878e74e216f349bbd5e73ad2c0
James Longo <v-jameslongo@microsoft.com>
2024-10-22 17:59:08 -0400

    updated ext_lib presets; updated rl_sim.sh to execute the correct rl_sim_cpp binary

--- OnlineTrainerExe/rl_sim.sh
+++ OnlineTrainerExe/rl_sim.sh
@@ -4,7 +4,36 @@ if [ -z "$RL_SIM_CONFIG" ]; then
   exit 1
 fi
 
-echo "$RL_SIM_CONFIG" > rl_sim_config.json
+echo "$RL_SIM_CONFIG" >rl_sim_config.json
 
-echo "Running RL_SIM..."
-rl_sim/rl_sim-linux-x64 -j rl_sim_config.json
+# Define the base path for the executable
+BASE_PATH="./rl_sim"
+
+# Determine the operating system
+OS=$(uname -s)
+
+# Determine the appropriate executable based on the operating system
+case "${OS}" in
+  Linux*)
+    EXECUTABLE="$BASE_PATH/rl_sim-linux-x64"
+    ;;
+  Darwin*)
+    EXECUTABLE="$BASE_PATH/rl_sim-macos-x64"
+    ;;
+  CYGWIN* | MINGW* | MSYS*)
+    EXECUTABLE="$BASE_PATH/rl_sim-win-x64.exe"
+    ;;
+  *)
+    echo "Unsupported OS: ${OS}"
+    exit 1
+    ;;
+esac
+
+echo "Detected rl_sim for $OS: $EXECUTABLE"
+# Check if the executable exists and run it
+if [ -f "$EXECUTABLE" ]; then
+  $EXECUTABLE -j rl_sim_config.json
+else
+  echo "Executable not found: $EXECUTABLE"
+  exit 1
+fi
